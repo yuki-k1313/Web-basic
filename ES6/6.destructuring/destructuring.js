9afa1e9d039f9fa4f911718f202db55210ae74ff
yuki-k1313 <yuki1313@naver.com>
2025-02-19 16:43:32 +0900

    2025-02-19

--- ES6/6.destructuring/destructuring.js
+++ ES6/6.destructuring/destructuring.js
@@ -1,43 +1,84 @@
 /*
     디스트럭처링 :
-    - 
-    - 
+    - 구조화된 배열 혹은 객체를 비구조화, 구조화 파괴하여 개별 변수로 할당하는 것
+    - 배열 혹은 객체에서 필요한 데이터만 추출해서 변수로 사용할 수 있도록 함
 */
+
+let fruits = ['apple', 'banana', 'cacao'];
+// const fruit1 = fruits[0];
+// const fruit2 = fruits[1];
+// const fruit3 = fruits[2];
+
+let king = {
+    name: '이성계',
+    ho: '태조',
+    address: '고려'
+};
+
+// const name = king.name;
+// const ho = king.ho;
+// const address = king.address;
 
 /*
     배열 디스트럭처링
-    - 
+    - 배열의 각각의 요소를 배열로부터 추출해서 변수에 할당
     - 
 */
 
-
+// fruits = ['apple', 'banana', 'cacao'];
+const [fruit1, fruit2, fruit3] = fruits;
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
 
 console.log('========================================');
 
 /*
     spread 연산자를 활용
-    - 
+    - 배열 디스트럭처링 마짖막에 스프레드 연산자를 사용하면 남은 모든 요소로 새로운 배열을 생성
 */
 
+// fruits = ['apple', 'banana', 'cacao'];
+const [anotherFruis, ...othersFruits] = fruits;
 
+console.log(anotherFruis);
+console.log(othersFruits);
 
 console.log('========================================');
 
 /*
     객체 디스트럭처링
-    - 
-    - 
+    - 객체의 각 속성을 객체로부터 추출하여 변수에 할당
+    - 할당 기준은 키
 */
 
+let king2 = {
+    name: '이방원',
+    ho: '태종',
+    address: '고려'
+};
 
+const { name: name1, ho: ho1, address: address1} = king;
+const { name, ho, address} = king2;
+
+console.log(name); 
+console.log(ho);
+console.log(address);
+
+console.log(name1);
+console.log(ho1);
+console.log(address1);
 
 console.log('========================================');
 
 /*
     spread 연산자를 활용
-    - 
+    - 객체 디스트럭처링 마지막 스프레드 연산자를 사용하면 나머지 모든 속성을 묶어서 새로운 객체를 생성
 */
 
+const { address: address2, ...kingInfo} = king;
+console.log(address2);
+console.log(kingInfo);
 
 
 console.log('========================================');
--- ES6/7.class/class.js
+++ ES6/7.class/class.js
@@ -1,59 +1,185 @@
 /*
-    클래스 정의 : 
-    - 
-    - 
-    - 
+    클래스 정의 : class 키워드로 클래스를 정의
+    - constructor 생성자를 이용해서 멤버 변수를 선언 및 초기화 가능
+    - 생성자에서 this를 사용하여 멤버 변수를 지정할 수 있음
+    - new 연산자로 인스턴스 생성가능
 */
 
+// 왕 클래스 : 이름 / 묘호(호) / 생일 / 사망일 
+class King {
+    constructor() {
+        this.name = '이성계';
+        this.ho = '태조';
+        this.birth = '1335-11-04';
+        this.death = '1408-06-27';
+    }
+}
 
+const teajo = new King();
+console.log(teajo);
+console.log('이름 : ', teajo.name);
 
 console.log('==================================================');
 
 /*
     constructor: 
-    - 
-    - 
-    - 
-    - 
+    - 인스턴스를 생성하고 클래스의 필드를 초기화하는 용도로 사용되는 특수한 메서드 ( 생성자 )
+    - 생성자 안에서 this 키워드로 클래스의 멤버 변수를 선언하고 초기화할 수 있음
+    - 생성자의 매개변수로 각 속석의 값을 받아와서 초기화
+    - 자바스크립트의 생성자는 오버로딩이 안됨
 */
 
+// class King {
+         // constructor()
+//     constructor(name, ho, birth, death) {
+//         this.name = name;
+//         this.ho = ho;
+//         this.birth = birth;
+//         this.death = death; 
+//     }
+// }
 
+// const jeongjong = new King('이방과', '정종', '1357-07-26', '1419-10-15');
+// console.log(jeongjong);
 
 console.log('==================================================');
 
 /*
-    클래스의 필드 선언 
+    클래스의 필드 선언 : 퍼블릭 - 프라이빗 / 인스턴스 - 클래스
+    - 인스턴스 퍼블릭 필드 
+    - 인스턴스 프라이빗 필드 
+    - 클래스 퍼블릭 필드 
+    - 클래스 프라이빗 필드 
 */ 
 
+class Sample1 {
+    // 인스턴스 퍼블릭 필드
+    publicField;
+    // 인스턴스 프라이빗 필드
+    #privateField;
+    // 클래스 퍼블릭 필드
+    static staticPublicField;
+    // 클래스 프라이빗 필드
+    static #staticPrivateField;
 
+    constructor() {
+        this.publicField = '퍼블릭';
+        this.#privateField = '프라이빗';
+        // 클래스 변수는 this. 으로 접근 불가능
+        // 아래와 같이
+        //
+    }
+}
+
+const instance1 = new Sample1();
+console.log(instance1);
+// console.log(instance1. #privateField);
+Sample1.staticPublicField = '스태틱';
+console.log(Sample1.staticPublicField);
 
 console.log('==================================================');
 
 /*
     getter, setter : 
-    - 
-    - 
+    - getter : 프라이빗 멤버 변수의 값을 얻을 수 있는 캡슐화 메서드
+    - setter : 프라이빗 멤버 변수의 값을 변경할 수 있는 캡슐화 메서드
 */
 
+class Sample2 {
+    #privateField;
 
+    constructor(privateField) {
+        this.#privateField = privateField;
+    }
+
+    get privateField() {
+        return this.#privateField;
+    }
+
+    set privateField(privateField) {
+        this.#privateField = privateField;
+    }
+}
+
+const instance2 = new Sample2('프라이빗');
+console.log(instance2);
+console.log(instance2.privateField);
+instance2.privateField = '변경 프라이빗 필드';
+console.log(instance2.privateField);
 
 console.log('==================================================');
 
 /*
     인스턴스 메서드와 정적 메서드 : 
-    - 
-    - 
+    - 클래스 내부에서 function 키워드 없이 함수를 작성하여 인스턴스 메서드 선언
+    - static 키워드를 추가하여 클래스 메서드를 선언
 */
 
+class Sample3 {
 
+    instanceField;
+    static classField = '클래스';
+
+    instanceMethod() {
+        console.log('인스턴스 메서드');
+    }
+
+    static classMethod() {
+        console.log('클래스 메서드');
+    }
+
+}
+
+const instance3 = new Sample3();
+instance3.instanceMethod();
+// instance3.classMethod();
+// console.log(instance3.classField);
+Sample3.classMethod();
 
 console.log('==================================================');
 
 /*
     클래스 상속 : 
-    - 
-    - 
-    - 
+    - extends 키워드를 사용하여 클래스 상속
+    - 메서드 오버라이딩 가능
+    - super 키워드로 부모 클래스 참조 가능
 */
 
+class ParentClass {
 
+    parentField;
+
+    constructor(parentField) {
+        this.parentField = parentField;
+    }
+
+    parentMethod() {
+        console.log('부모 메서드');
+    }
+
+}
+
+class ChildClass extends ParentClass {
+
+    childField;
+
+    constructor(parentField, childField) {
+        super(parentField)
+        this.childField = childField;
+    }
+
+    childMethod() {
+        console.log('자식 메서드');
+    }
+
+    parentMethod () {
+        super.parentMethod();
+        console.log('오버라이딩 메서드');
+    }
+
+}
+
+const childInstance = new ChildClass('부모', '자식');
+console.log(childInstance);
+childInstance.childMethod();
+childInstance.parentMethod();
